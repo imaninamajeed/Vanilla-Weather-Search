605f98a06484d02dd5ed1caa345d11a979e8458e
imaninamajeed <imaninamajeed97@gmail.com>
2024-02-16 15:10:23 +0800

    refactor: Test Weather API integration

--- index.js
+++ index.js
@@ -6,18 +6,32 @@
 // The default form submission behavior is allowed
 // Potentially causing the page to reload or navigate to a new page,
 // which could disrupt the intended functionality of the web application, such as updating cityElement.innerHTML.
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
 
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
-	let cityElement = document.querySelector("#weather-app-city");
-
-	let inputValue = searchInput.value;
-	var capitalizedValue =
-		inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-
-	cityElement.innerHTML = capitalizedValue;
+	searchCity(searchInput.value);
+	// let cityElement = document.querySelector("#weather-app-city");
+	// let inputValue = searchInput.value;
+	// var capitalizedValue =
+	// 	inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+	// cityElement.innerHTML = capitalizedValue;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function searchCity(city) {
+	let apiKey = "b2a5adcct04b33178913oc335f405433";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+	axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#weather-app-temperature");
+	let temperature = response.data.temperature.current;
+	alert(temperature);
+	let cityElement = document.querySelector("#weather-app-city");
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = Math.round(temperature);
+}
